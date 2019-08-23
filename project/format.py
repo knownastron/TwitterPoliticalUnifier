617306512ae1a91bda8570a393df9f478d5bf999
knownastron <knownastron@gmail.com>
2019-08-23 11:38:20 -0600

    added formatTweetText, format_username

--- project/format.py
+++ project/format.py
@@ -1,4 +1,13 @@
 def formatTweetText(text):
+    '''
+    replaces newline character with a space
+    '''
     text = text.replace("\n", " ")
 
     return text
+
+def format_username(username):
+    if username[0] != '@':
+        return '@' + username
+    else:
+        return username
