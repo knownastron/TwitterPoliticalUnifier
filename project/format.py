b8dca14ea8900821f251ad2a8d01092625279d54
knownastron <knownastron@gmail.com>
2019-08-26 09:03:08 -0600

    added more format_text options

--- project/format.py
+++ project/format.py
@@ -1,13 +1,38 @@
-def formatTweetText(text):
-    '''
-    replaces newline character with a space
-    '''
-    text = text.replace("\n", " ")
+import re
 
-    return text
+
+def format_tweet_text(text):
+    '''
+    replaces newline character with a space, removes links
+
+    :param text: the tweet text reformatted
+    :return:
+    '''
+
+    text = remove_new_line(text)
+    text = remove_hyperlinks(text)
+
+    return text.strip()
+
 
 def format_username(username):
     if username[0] != '@':
         return '@' + username
     else:
         return username
+
+
+def remove_new_line(text):
+    return text.replace("\n", " ")
+
+
+def remove_hyperlinks(text):
+    return re.sub(r"http\S+", "", text)
+
+
+def remove_hashtags(text):
+    return re.sub(r"#\S+", "", text)
+
+
+def remove_mentions(text):
+    return re.sub(r"@\S+", "", text)
