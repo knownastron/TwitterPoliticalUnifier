b4943e0dc742afe62248ee4d8dc06eda3d6eabd5
knownastron <knownastron@gmail.com>
2019-08-29 10:33:49 -0600

    .gitignore fix

--- project/driver.py
+++ project/driver.py
@@ -8,16 +8,21 @@ def get_username_and_tweet_id(url):
     username = format.format_username(split_url[index_of_twitter + 1])
     tweet_id = split_url[index_of_twitter + 3]
     return username, tweet_id
+
 url = 'https://twitter.com/RonPaul/status/1164231385676222464'
 username, tweet_id = get_username_and_tweet_id(url)
 twit_conn = tf.TwitterConnection()
 neo4j_conn = n4j.Neo4JConnection()
 
-liked_list = twit_conn.get_liked_list(tweet_id)
 
-for user in liked_list:
-    print(user, username)
-    neo4j_conn.create_liked_relationship(user, username)
+ids = ['1166494782904000512', '1166502815717568512', '1166396704620347392', '1128380233910894592']
+
+for id in ids:
+    liked_list = twit_conn.get_liked_list(id)
+
+    for user in liked_list:
+        print(user, username)
+        neo4j_conn.create_liked_relationship(user, username)
     
 
 
