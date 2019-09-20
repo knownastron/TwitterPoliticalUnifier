175ecdf2df9a8dd9e42528e9445fd7025a264e6d
knownastron <knownastron@gmail.com>
2019-09-20 16:10:18 -0600

    added a get_num_tweets_by, get_users_by_pol_label

--- project/Format.py
+++ project/Format.py
@@ -1,41 +1,58 @@
 import re
 
 
-def format_tweet_text(text):
-    '''
-    replaces newline character with a space, removes links
+class Format():
+    @staticmethod
+    def format_tweet_text(text):
+        '''
+        replaces newline character with a space, removes links
 
-    :param text: the tweet text reformatted
-    :return:
-    '''
+        :param text: the tweet text reformatted
+        :return:
+        '''
 
-    text = remove_new_line(text)
-    text = remove_hyperlinks(text)
+        text = remove_new_line(text)
+        text = remove_hyperlinks(text)
 
-    return text.strip()
+        return text.strip()
 
+    @staticmethod
+    def format_username(username):
+        if username[0] != '@':
+            return '@' + username
+        else:
+            return username
 
-def format_username(username):
-    if username[0] != '@':
-        return '@' + username
-    else:
-        return username
+    @staticmethod
+    def remove_new_line(text):
+        return text.replace("\n", " ")
 
+    @staticmethod
+    def remove_hyperlinks(text):
+        return re.sub(r"http\S+", "", text)
 
-def remove_new_line(text):
-    return text.replace("\n", " ")
+    @staticmethod
+    def remove_hashtags(text):
+        return re.sub(r"#\S+", "", text)
 
+    @staticmethod
+    def remove_mentions(text):
+        return re.sub(r"@\S+", "", text)
 
-def remove_hyperlinks(text):
-    return re.sub(r"http\S+", "", text)
+    @staticmethod
+    def remove_picture_links(text):
+        return re.sub(r"pic.twitter.com\S+", "", text)
 
+    @staticmethod
+    def remove_hashtags(text):
+        return re.sub(r"#[\w]*", "", text)
 
-def remove_hashtags(text):
-    return re.sub(r"#\S+", "", text)
+    @staticmethod
+    def remove_mentions(text):
+        return re.sub(r"@[\w]*", "", text)
 
+    @staticmethod
+    def remove_punctuation(text):
+        return text.translate(str.maketrans('','',string.punctuation + '—' + '“' + '…'))
 
-def remove_mentions(text):
-    return re.sub(r"@\S+", "", text)
-
-def remove_picture_links(text):
-    return re.sub(r"pic.twitter.com\S+", "", text)
+    
