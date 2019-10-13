8ef4cd6376dab2154cb5d1f299c6dc5afd0afa6f
knownastron <knownastron@gmail.com>
2019-10-12 22:22:13 -0600

    initial commit, working header

---
+++ project/reactapp/src/components/TweetInputForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './TweetInputForm.css'
+
+class TweetInputForm extends React.Component {
+
+  render() {
+    return (
+      <header className='header'>Twitter Political Unifier </header>
+      // Form
+    );
+  }
+}
+
+export default TweetInputForm;
