0a4dd2933badca0906b1c3fb4ae773b77957d661
knownastron <knownastron@gmail.com>
2019-10-17 15:18:15 -0600

    initial commit

---
+++ project/reactapp/src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+class About extends React.Component {
+
+  render() {
+    return (
+      <div className="component-main-div">
+        <p> This is the about page. Welcome and goodbye. </p>
+      </div>
+    );
+  }
+}
+
+export default About;
