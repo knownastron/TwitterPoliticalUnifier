5b652cbb4d27824ae0d5cc90fff1701b374adb0c
knownastron <knownastron@gmail.com>
2019-10-17 11:10:18 -0600

    added routes

--- project/reactapp/src/App.js
+++ project/reactapp/src/App.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header'
+import About from './components/About'
+import LabelUser from './components/LabelUser'
 import TweetInputForm from './components/TweetInputForm'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div className="AppBody">
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="AppBody">
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <TweetInputForm />
+            <LabelUser />
+          </Route>
 
-      <TweetInputForm />
+        </Switch>
+
+
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
