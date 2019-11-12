4bb6bd0053b440f25c7385f37739c63929fecd30
knownastron <knownastron@gmail.com>
2019-11-11 22:47:08 -0700

    added th and td and format date

--- project/reactapp/src/components/SearchedUserItem.js
+++ project/reactapp/src/components/SearchedUserItem.js
@@ -6,28 +6,40 @@ class SearchedUserItem extends React.Component {
   }
 
   componentDidMount() {
-    // const url = 'http://127.0.0.1:5000/api/2.0/getsearchedusers';
-    // let self = this;
-    // axios.post(url, JSON.stringify({
-    //   // 'token': token
-    //   'email': 'knownastron@gmail.com'
-    // }), {headers: {'Content-Type': 'application/json;charset=UTF-8'}})
-    // .then(function(response) {
-    //   console.log(response.data.searchedUsers)
-    //   self.setState({users: response.data.searchedUsers})
-    // })
-    // .catch(function (error) {
-    //   console.log(error)
-    // });
+
   }
 
   render() {
+    let screen_name = this.props.user.screenName;
+    let date = new Date(this.props.user.searchDate)
+    let formattedDate = formatDate(date)
     return (
-      <div className='searched-user-item-div'>
-        <a href={'twitterUser/' + this.props.user.screenName}> {this.props.user.screenName} </a><h5>{this.props.user.searchDate} </h5>
-      </div>
+      // <div className='searched-user-item-div'>
+        <tr>
+          <td className='user-td'><a href={'https://www.twitter.com/' + screen_name} > {'@' + screen_name} </a></td>
+          <td className='user-td'>{formattedDate}</td>
+          <td className='user-td'>{this.props.user.polLabel}</td>
+        </tr>
+      // </div>
     )
   }
 }
+
+function formatDate(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  let month = date.getMonth()+1
+  month = (month < 10 ? '0' + month.toString() : month);
+  let day = date.getDate()
+  day = (day < 10 ? '0' + day.toString() : day)
+  return month + "/" + day + "/" + date.getFullYear() + "  " + strTime;
+}
+
+
 
 export default SearchedUserItem;
