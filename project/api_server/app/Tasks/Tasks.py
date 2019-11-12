9ab0a86e5160fe8f3c95f78a2075d702cf919865
knownastron <knownastron@gmail.com>
2019-11-11 17:11:44 -0700

    added get_searched_users, create_new_user, and predict_user tasks

--- project/api_server/app/Tasks/Tasks.py
+++ project/api_server/app/Tasks/Tasks.py
@@ -1,7 +1,17 @@
 import random
 import time
+import datetime
+import joblib
 import celery
 from celery import Celery
+from Database import SQLConnection
+from Database import mysql_aws_credentials
+from Services import TwitterScraper
+from Services import Format
+from Services import TwitterConnection
+
+#temp
+import os
 
 config = {
     'CELERY_BROKER_URL': 'redis://localhost:6379/0',
@@ -9,8 +19,27 @@ config = {
 }
 
 #Set up Celery
-celery = Celery('CeleryTest', broker=config['CELERY_BROKER_URL'])
+celery = Celery('Tasks', broker=config['CELERY_BROKER_URL'])
 celery.conf.update(config)
+
+#Set up db
+aws_conn = SQLConnection.AWSConnection(mysql_aws_credentials.HOST, mysql_aws_credentials.PORT, mysql_aws_credentials.DATABASE_NAME,
+                         mysql_aws_credentials.USER, mysql_aws_credentials.PASSWORD)
+
+#Set up TwitterScrapper and TwitterConnection
+rate_delay_seconds = 5
+error_delay_seconds = 10
+twit_scraper = TwitterScraper.TwitterSearchImpl(rate_delay_seconds, error_delay_seconds, None)
+
+twit_conn = TwitterConnection.TwitterConnection()
+
+#set up NLP and vectorizer
+nlp_model = open('./NLP_political_classifier.pkl', 'rb')
+clf = joblib.load(nlp_model)
+
+vectorizer_object = open('./tfid_vectorizer.pkl', 'rb')
+vectorizer = joblib.load(vectorizer_object)
+
 
 
 @celery.task(bind=True)
@@ -36,19 +65,53 @@ def long_task(self):
     return {'current': 100, 'total': 100, 'status': 'Task completed!',
             'result': 42}
 
-@celery.task(bind=True)
-def predict_user(self, screen_name, app_user):
-    pass
-    # if screen_name already in db,
-        # update app_user's profile
 
-    # else
-        #scrape tweets for the user
-        #run it through the NLP
-        #write results to the DB
+@celery.task(bind=True)
+def predict_user(self, screen_name, app_user_email):
+    cur_user = aws_conn.get_twitter_user(screen_name)
+    time_of_search = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    if cur_user:
+        aws_conn.insert_searched_twitter_user(app_user_email, screen_name, time_of_search)
+        return {'status': 'already existed' }
+
+
+    user_id = twit_conn.get_user_ids([screen_name])[0][1]
+    print('GOT USER ID', user_id)
+    search_query = "from:" + screen_name
+    twit_scraper.search(search_query)
+
+    tweets = twit_scraper.get_raw_tweets()
+    tweets = ' '.join(tweets)
+    print('GOT TWEETS')
+
+    cleaned_text = Format.Format.denoise_tweet(tweets)
+    cleaned_text = Format.Format.stem_words_str(cleaned_text)
+    print('CLEANED TWEETS')
+
+    vectorized_text = vectorizer.transform([cleaned_text])
+    prediction = clf.predict(vectorized_text)[0]
+    print('PREDICTED', prediction)
+
+    aws_conn.write_twitter_user(screen_name, int(user_id), user_id, prediction)
+    print('WROTE NEW USER')
+    aws_conn.insert_searched_twitter_user(app_user_email, screen_name, time_of_search)
+    print('WROTE SEARCHED')
+    return {'status': 'added new user'}
+
+@celery.task(bind=True)
+def get_searched_users(self, app_user_email):
+    print('entered get_searched_users')
+    searched_users = aws_conn.get_searched_twitter_users(app_user_email)
+    return searched_users
+
+@celery.task(bind=True)
+def create_new_user(self, email):
+    aws_conn.create_user_profile(email)
+    return
 
 @celery.task(bind=True)
 def predict_tweet(self, screen_name, tweet_id, app_user):
+    pass
     #if tweet already in db,
         # update app_user's profile
 
@@ -57,6 +120,3 @@ def predict_tweet(self, screen_name, tweet_id, app_user):
         # scrape tweets of all users
         # run it through NLP
         # update app_user's profile
-
-
-
