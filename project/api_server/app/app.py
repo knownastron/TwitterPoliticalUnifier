abbe93d9bae85e7ce2f863c0410c4f41d6c4557e
knownastron <knownastron@gmail.com>
2019-11-11 17:10:33 -0700

    added 2.0 routes

--- project/api_server/app/app.py
+++ project/api_server/app/app.py
@@ -10,7 +10,7 @@ import os #remove later
 import json
 from Services import Format
 from Services import TwitterScraper
-from Tasks import CeleryTest
+from Tasks import Tasks
 from Services import Authentication
 from Services import Responses
 from celery import Celery
@@ -45,7 +45,8 @@ def index():
 @app.route('/longtask', methods=['POST'])
 def longtask():
     print('entered longtask route')
-    task = CeleryTest.long_task.apply_async()
+    task = Tasks.long_task.apply_async()
+    task.wait()
     return str(status.HTTP_202_ACCEPTED) # jsonify({}), 202, {'Location': url_for('taskstatus', task_id=task.id)}
 
 
@@ -75,7 +76,7 @@ def predictText():
 
 
 @app.route('/api/1.0/labeluser', methods = ['POST'])
-def predictUser():
+def predictUser1():
     data = request.get_json()
     verify = Authentication.check_jwt(data, None)
 
@@ -110,6 +111,54 @@ def predictUser():
     return {'username': username, 'politicalLabel': prediction[0]}
 
 
+@app.route('/api/2.0/getsearchedusers', methods=['POST'])
+def get_searched_users():
+    data = request.get_json()
+    # claims = Authentication.check_jwt(data, None)
+    # email = claims['email']
+    email = data['email']
+
+    task = Tasks.get_searched_users.apply_async([email])
+    task.wait()
+    result = task.result
+    print(result)
+    ret = {'searchedUsers': []}
+
+    for i, user in enumerate(result):
+        print(user)
+        ret['searchedUsers'].append({'id': i, 'screenName': user[0], 'searchDate': user[2], 'polLabel': user[5]})
+
+    return jsonify(ret)
+
+@app.route('/api/2.0/labeluser', methods = ['POST'])
+def predictUser2():
+    data = request.get_json()
+    # task = Tasks.short_task.delay(data['username'])
+    # print(data)
+    # verify = Authentication.check_jwt(data, None)
+    verify = True # remove later
+    if not verify:
+        return auth_error()
+
+    task = Tasks.predict_user.apply_async([data['username'], data['email']])
+    # task.wait()
+
+    resp = {'status_code': 401}
+    return resp
+
+
+@app.route('/api/2.0/createnewuser', methods = ['POST'])
+def create_new_user():
+    # insert check token
+    data = request.get_json()
+    email = data['email']
+
+    Tasks.create_new_user.apply_async([email])
+
+    resp = {'status_code': 200}
+    return resp
+
+
 '''
 @app.route('/labelTweet', methods=['POST'])
 def labelTweet():
@@ -120,29 +169,27 @@ def labelTweet():
     return 200 OK;
 '''
 
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+            'status': 404,
+            'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
 
-@celery.task(bind=True)
-def long_task(self):
-    """Background task that runs a long function with progress reports."""
-    verb = ['Starting up', 'Booting', 'Repairing', 'Loading', 'Checking']
-    adjective = ['master', 'radiant', 'silent', 'harmonic', 'fast']
-    noun = ['solar array', 'particle reshaper', 'cosmic ray', 'orbiter', 'bit']
-    message = ''
-    total = random.randint(10, 50)
+    return resp
 
-    print('entered long task function')
-    for i in range(total):
-        print(i)
-        if not message or random.random() < 0.25:
-            message = '{0} {1} {2}...'.format(random.choice(verb),
-                                              random.choice(adjective),
-                                              random.choice(noun))
-        self.update_state(state='PROGRESS',
-                          meta={'current': i, 'total': total,
-                                'status': message})
-        time.sleep(1)
-    return {'current': 100, 'total': 100, 'status': 'Task completed!',
-            'result': 42}
+@app.errorhandler(401)
+def auth_error(error=None):
+    message = {
+        'status': 401,
+        'message': 'Authentication Error'
+    }
+    resp = jsonify(message)
+    resp.status_code = 401
+
+    return resp
 
 if __name__ == '__main__':
    app.run(debug="True", threaded=True)
