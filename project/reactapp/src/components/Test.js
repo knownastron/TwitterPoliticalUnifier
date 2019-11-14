13f253a863d1c3c439e5d6ff9e6f0c49e8f7844b
knownastron <knownastron@gmail.com>
2019-11-14 12:33:19 -0700

    added TwitterTweetEmbed

--- project/reactapp/src/components/Test.js
+++ project/reactapp/src/components/Test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Auth } from 'aws-amplify';
+import { TwitterTweetEmbed } from 'react-twitter-embed'
 
 class Test extends React.Component {
   onClick = (e) => {
@@ -10,13 +11,32 @@ class Test extends React.Component {
     .catch(err => console.log(err));
   }
 
+
   render() {
     return (
-      <div className="component-main-div">
-        <button type="button" onClick={this.onClick}> CLICK</button>
-      </ div>
+      <div id="test-div">
+        <TwitterTweetEmbed options={
+            {
+              width: 300,
+              cards: 'hidden'
+            }
+          }
+          tweetId={'1000000245584072704'}
+        />
+        <TwitterTweetEmbed
+          tweetId={'1194120382413803520'}
+          options={
+              {
+                width: 300,
+                cards: 'hidden'
+              }
+            }
+        />
+      </div>
     );
   };
 }
+
+var muh_style = {'height': '100px'}
 
 export default Test;
