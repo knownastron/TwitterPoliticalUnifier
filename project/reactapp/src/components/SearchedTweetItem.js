df3e5d3941f4028a9b0e71a1a88a78bf238420ba
knownastron <knownastron@gmail.com>
2019-11-22 14:33:51 -0700

    working implementation

---
+++ project/reactapp/src/components/SearchedTweetItem.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import {
+  Link
+} from "react-router-dom"
+
+class SearchedTweetItem extends React.Component {
+  state = {
+    tweets: []
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    let screen_name = this.props.tweet.screenName;
+    let tweetId = this.props.tweet.tweetId
+    let date = new Date(this.props.tweet.searchDate)
+    let formattedDate = formatDate(date)
+    let inProgress = this.props.tweet.inProgress
+    return (
+        <tr>
+          <td className='user-td'><a href={'https://www.twitter.com/' + screen_name} > {'@' + screen_name} </a></td>
+          <td className='user-td'><a href={'https://twitter.com/' + screen_name + '/status/' + tweetId}>Go to Tweet</a></td>
+          <td className='user-td'>{formattedDate}</td>
+          <td className='user-td'>{ inProgress ? 'In progress...' : <Link to={'/tweetdetails/' + tweetId}>Details</Link>}</td>
+        </tr>
+    )
+  }
+}
+
+function formatDate(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  let month = date.getMonth()+1
+  month = (month < 10 ? '0' + month.toString() : month);
+  let day = date.getDate()
+  day = (day < 10 ? '0' + day.toString() : day)
+  return month + "/" + day + "/" + date.getFullYear() + "  " + strTime;
+}
+
+
+
+export default SearchedTweetItem;
---
+++ project/reactapp/src/components/SearchedTweets.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import SearchedTweetItem from './SearchedTweetItem';
+
+class SearchedTweets extends React.Component {
+  state = {
+    tweets: []
+  }
+
+  componentDidMount() {
+    const url = 'http://127.0.0.1:5000/api/2.0/getsearchedtweets';
+    let self = this;
+    axios.post(url, JSON.stringify({
+      // 'token': token
+      // 'email': this.props.email
+      'email': 'knownastron@gmail.com'
+    }), {headers: {'Content-Type': 'application/json;charset=UTF-8'}})
+    .then(function(response) {
+      console.log(response.data.searchedUsers)
+      self.setState({tweets: response.data.searchedTweets})
+    })
+    .catch(function (error) {
+      console.log(error)
+    });
+  }
+
+  render() {
+    return (
+      <div className="component-main-div">
+        <h1> Searched Tweet History </h1>
+        <table className="user-table">
+            <tbody>
+              <tr>
+                <th className='user-th'>Author</th>
+                <th className='user-th'>TweetId</th>
+                <th className='user-th'>Date Searched</th>
+                <th className='user-th'>Status</th>
+              </tr>
+              {
+                this.state.tweets.map((tweet) => (
+                  <SearchedTweetItem key={tweet.id} tweet={tweet} />
+                ))
+              }
+          </tbody>
+        </ table>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated,
+  email: state.auth.email,
+  token: state.auth.token
+})
+
+
+
+export default connect(mapStateToProps, { }) (SearchedTweets);
