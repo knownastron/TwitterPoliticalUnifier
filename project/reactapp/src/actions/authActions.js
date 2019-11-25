8a0645e1c0de29cbd275ccb8de09a60d3f84c61a
knownastron <knownastron@gmail.com>
2019-11-25 13:40:02 -0700

    added dispatch for USER_NOT_CONFIRMED

--- project/reactapp/src/actions/authActions.js
+++ project/reactapp/src/actions/authActions.js
@@ -1,4 +1,7 @@
-import { LOGIN_USER_SUCCESS, LOGOUT_USER_SUCCESS } from './types';
+import { LOGIN_USER_SUCCESS,
+         LOGOUT_USER_SUCCESS,
+         USER_NOT_CONFIRMED,
+         RESET_USER_CONFIRMATION} from './types';
 import { Auth } from 'aws-amplify';
 
 export const loginUser = (loginInfo) => async dispatch => {
@@ -16,6 +19,11 @@ export const loginUser = (loginInfo) => async dispatch => {
     alert("Logged in");
   }).catch(err => {
     if (err.code === 'UserNotConfirmedException') {
+      dispatch({
+        type: USER_NOT_CONFIRMED,
+        payload: loginInfo,
+        confirmationRequired: true
+      })
       alert("User has not been confirmed")
       // need error handling redirect to verify page
       return
@@ -36,4 +44,11 @@ export const logoutUser = () => async dispatch => {
       alert('Log out successful')
     })
     .catch(err => console.log(err));
+}
+
+export const resetConfirmation = () => dispatch => {
+  dispatch({
+    type: RESET_USER_CONFIRMATION,
+    confirmationRequired: false
+  })
 }
