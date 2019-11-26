53a80cdbaaf0cdbd396000ddce9c2d78b551fe49
knownastron <knownastron@gmail.com>
2019-11-25 21:11:53 -0700

    initial commit with blank h1 tag

---
+++ project/reactapp/src/components/LandingPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+class LandingPage extends React.Component {
+
+  render() {
+    return (
+      <div className="landing-page-div">
+        <div id='landing-page-h1'>
+          <h1> </h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
