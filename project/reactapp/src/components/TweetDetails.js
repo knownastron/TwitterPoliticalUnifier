6a2fae674e2da91c58d102b5de56b96f9795615c
knownastron <knownastron@gmail.com>
2019-12-04 00:46:36 -0700

    added map and markers and geocodio integration

--- project/reactapp/src/components/TweetDetails.js
+++ project/reactapp/src/components/TweetDetails.js
@@ -3,32 +3,121 @@ import axios from 'axios';
 import { connect } from 'react-redux';
 import SearchedUserItem from './SearchedUserItem';
 import { TwitterTweetEmbed } from 'react-twitter-embed'
+import config from '../config/config';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
+const gop = new L.Icon({
+    iconUrl: require('../images/gop.png'),
+    iconRetinaUrl: require('../images/gop.png'),
+    iconAnchor: null,
+    popupAnchor: null,
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(20, 18),
+    className: 'leaflet-div-icon'
+});
+
+const dem = new L.Icon({
+    iconUrl: require('../images/dem.png'),
+    iconRetinaUrl: require('../images/dem.png'),
+    iconAnchor: null,
+    popupAnchor: null,
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(20, 20),
+    className: 'leaflet-div-icon'
+});
 
 class TweetDetails extends React.Component {
   state = {
-    users: []
+    users: [],
+    locations: [],
+    userInfoWithGeo: [],
+    lat: 39.8283,
+    lng: -98.5795,
+    zoom: 4
   }
+
   componentDidMount() {
+    this.getTwitterUsers()
+  }
+
+  getGeoLocations = async (locations) => {
+    const codioUrl = 'https://api.geocod.io/v1.4/geocode?';
+    return axios.post(codioUrl + 'api_key=' + config.geocodio.KEY,
+        JSON.stringify(locations),
+        {headers: {'Content-Type': 'application/json;charset=UTF-8'}}
+      ).then(function(response) {
+        let geo_locations = [];
+        response.data.results.forEach(function(geo) {
+          if (geo.response.error) {
+            geo_locations.push({'error': 'error'})
+          } else {
+            geo_locations.push(geo.response.results[0].location)
+          }
+        }
+      )
+      return geo_locations;
+      })
+  }
+
+  getTwitterUsers = async () => {
     const url = 'http://127.0.0.1:5000/api/2.0/gettweetlikes';
     let self = this;
-    axios.post(url, JSON.stringify({
-      // 'token': token
+    await axios.post(url, JSON.stringify({
+      'token': this.props.token,
       // 'email': this.props.email
       'tweetId': this.props.match.params.id,
       'email': 'knownastron@gmail.com'
     }), {headers: {'Content-Type': 'application/json;charset=UTF-8'}})
     .then(function(response) {
-      console.log(response.data.searchedUsers)
       self.setState({users: response.data.tweetLikes})
     })
     .catch(function (error) {
       console.log(error)
     });
+
+    // gather all the locations and usernames
+    let userInfo = []
+    let locations = []
+    this.state.users.forEach(function(user){
+      if (user.location !== '') {
+        userInfo.push([user.screenName, user.polLabel])
+        locations.push(user.location)
+      }
+    })
+
+    // get geo location
+    let userInfoAndGeo = [];
+    await this.getGeoLocations(locations)
+      .then(locations => {
+        userInfoAndGeo = locations.map((loc, i) => {
+          return {screenName: userInfo[i][0],
+                   polLabel: userInfo[i][1],
+                   coordinates: loc};
+        })
+      })
+    console.log(userInfoAndGeo);
+    self.setState({userInfoWithGeo: userInfoAndGeo})
   }
 
-  render() {
-    return (
 
+
+  render() {
+    const position = [this.state.lat, this.state.lng];
+    return (
       <div className="component-main-div">
 
         <h1>Tweet Details for:</h1>
@@ -49,11 +138,35 @@ class TweetDetails extends React.Component {
                 }
             </tbody>
           </table>
+          <Map style={muh_style} center={position} zoom={this.state.zoom}>
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'/>
+            {
+              this.state.userInfoWithGeo.map((curUser, i) => (
+                curUser.coordinates.error ? null :
+                  curUser.polLabel === 'conservative' ?
+                  <Marker key={i} icon={gop} position={[curUser.coordinates.lat, curUser.coordinates.lng]}>
+                        // <Popup>
+                        //   <img src='threelinebutton.png'></img>
+                        // </Popup>
+                  </Marker> :
+                  <Marker key={i} icon={dem} position={[curUser.coordinates.lat, curUser.coordinates.lng]}>
+                        // <Popup>
+                        //   <img src='threelinebutton.png'></img>
+                        // </Popup>
+                  </Marker>
+
+              ))
+            }
+          </Map>
 
       </div>
     )
   }
 }
+
+var muh_style = {'height': '400px', width: '100%'}
 
 const mapStateToProps = state => ({
   isAuthenticated: state.auth.isAuthenticated,
