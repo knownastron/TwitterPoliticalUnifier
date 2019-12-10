8cd969d5bc5cfeed4aff88e4aba3ef60a6bbac37
knownastron <knownastron@gmail.com>
2019-12-09 18:58:08 -0700

    added response message if 401

--- project/reactapp/src/components/SearchedTweets.js
+++ project/reactapp/src/components/SearchedTweets.js
@@ -13,17 +13,20 @@ class SearchedTweets extends React.Component {
     const url = 'http://127.0.0.1:5000/api/2.0/getsearchedtweets';
 
     let self = this;
+
     axios.post(url, JSON.stringify({
-      // 'token': token
-      // 'email': this.props.email
-      'email': 'knownastron@gmail.com'
+      'token': this.props.token,
+      'email': this.props.email
     }), {headers: {'Content-Type': 'application/json;charset=UTF-8'}})
     .then(function(response) {
-      console.log(response.data.searchedUsers)
+      if (response.data.statusCode === 401) {
+        alert(response.data.message)
+      }
       self.setState({tweets: response.data.searchedTweets})
     })
     .catch(function (error) {
       console.log(error)
+      alert('Something went wrong. Please try again')
     });
 
 
@@ -38,7 +41,7 @@ class SearchedTweets extends React.Component {
               <tbody>
                 <tr>
                   <th className='user-th'>Author</th>
-                  <th className='user-th'>TweetId</th>
+                  <th className='user-th'>Tweet</th>
                   <th className='user-th'>Date Searched</th>
                   <th className='user-th'>Status</th>
                 </tr>
