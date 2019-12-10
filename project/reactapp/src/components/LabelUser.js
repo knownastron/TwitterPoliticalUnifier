e3e23de50a0cfc1acb3499739e26ebe149f30061
knownastron <knownastron@gmail.com>
2019-12-09 18:49:49 -0700

    added redirectToDashboard and redirectToLogIn state

--- project/reactapp/src/components/LabelUser.js
+++ project/reactapp/src/components/LabelUser.js
@@ -1,42 +1,41 @@
 import React from 'react'
 import './LabelTweet.css'
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
 import axios from 'axios';
 
 class LabelUser extends React.Component {
   state = {
-    email: '',
-    username: ''
+    username: '',
+    redirectToLogIn: false,
+    redirectToDashboard: false
   };
 
-  validateEmail = (email) => {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
-  }
-
   submission = (e) => {
-    let token = this.props.token;
     e.preventDefault();
 
-    if (this.validateEmail(this.state.email)) {
-
-      // const url = 'https://www.knownastron.com:6001/api/2.0/labeluser';
-      const url = 'http://127.0.0.1:5000/api/2.0/labeluser';
-
-      axios.post(url, JSON.stringify({
-        ...this.state,
-        'token': token
-      }), {headers: {'Content-Type': 'application/json;charset=UTF-8'}})
-      .then(function(response) {
-        console.log(response)
-      })
-      .catch(function (error) {
-        console.log(error)
-      });
-
-    } else {
-      alert('Email error');
+    if (!this.props.isAuthenticated) {
+      alert('Please log in to use this feature.');
+      this.setState({redirectToLogIn: true})
+      return;
     }
 
+
+    // const url = 'https://www.knownastron.com:6001/api/2.0/labeluser';
+    const url = 'http://127.0.0.1:5000/api/2.0/labeluser';
+
+    axios.post(url, JSON.stringify({
+      'username': this.state.username,
+      'token': this.props.token,
+      'email': this.props.email
+    }), {headers: {'Content-Type': 'application/json;charset=UTF-8'}})
+    .then((response) => {
+      this.setState({redirectToDashboard: true})
+      console.log(response)
+    })
+    .catch(function (error) {
+      console.log(error)
+    });
   }
 
   emailChange = (e) => {
@@ -52,17 +51,16 @@ class LabelUser extends React.Component {
   }
 
   render() {
+    if (this.state.redirectToLogIn) {
+      return (<Redirect to='/login' />);
+    } else if (this.state.redirectToDashboard) {
+      return (<Redirect to='/dashboard' />);
+    }
     return (
-      <div className="component-main-div">
+      <div className="component-half-div">
         <h2> Twitter User's Political Affiliation </h2>
         <p> Discover a Twitter user's political leanings <br/> based on their tweet history</p>
         <form onSubmit={this.submission}>
-          <label htmlFor="email">Email</label>
-          <input type="text"
-            id="emailInput"
-            name="email"
-            placeholder="Email Address"
-            onChange={this.emailChange}/>
           <label htmlFor="username">Twitter Username</label>
           <input type="text"
             id="username"
@@ -78,7 +76,8 @@ class LabelUser extends React.Component {
 
 const mapStateToProps = state => ({
   isAuthenticated: state.auth.isAuthenticated,
-  token: state.auth.token
+  token: state.auth.token,
+  email: state.auth.email
 })
 
 export default connect(mapStateToProps, {})(LabelUser);
