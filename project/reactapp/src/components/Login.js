5650c824d683ae6b910f2cfde6e2bc61f5e229ec
knownastron <knownastron@gmail.com>
2019-12-09 19:11:48 -0700

    removed blank line

--- project/reactapp/src/components/Login.js
+++ project/reactapp/src/components/Login.js
@@ -41,7 +41,6 @@ class Login extends React.Component {
     this.setState({createNewUser: true})
   }
 
-
   render() {
     if (this.props.isAuthenticated) {
       return (< Redirect to="/dashboard" />)
