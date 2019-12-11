f5552c641d42ac02c0364f02146f0e8acca7f618
knownastron <knownastron@gmail.com>
2019-12-10 19:03:10 -0700

    test deployment

---
+++ project/reactapp/build1/precache-manifest.0fd795847bfe77556d847ac7ccbdbdff.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d81777fbd195aaf95c3e84536c762182",
+    "url": "/index.html"
+  },
+  {
+    "revision": "520dd7a085e4577def9e",
+    "url": "/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "691c58822b5ea2fa5772",
+    "url": "/static/css/main.3131dd08.chunk.css"
+  },
+  {
+    "revision": "520dd7a085e4577def9e",
+    "url": "/static/js/2.9c900c2c.chunk.js"
+  },
+  {
+    "revision": "691c58822b5ea2fa5772",
+    "url": "/static/js/main.469c7498.chunk.js"
+  },
+  {
+    "revision": "756540ff4c09f63af14c",
+    "url": "/static/js/runtime-main.85222b5f.js"
+  },
+  {
+    "revision": "7b02885c11f405195fb16ec06cd766b5",
+    "url": "/static/media/dem.7b02885c.png"
+  },
+  {
+    "revision": "3ca98858abbcae0e2e08bb3dc84c30ba",
+    "url": "/static/media/epluribusunum.3ca98858.png"
+  },
+  {
+    "revision": "ecd091859b2a0f2c2c2ff6422897f01e",
+    "url": "/static/media/gop.ecd09185.png"
+  },
+  {
+    "revision": "7abd4dd3eebcf66b3a2ee6bc1f6f2e4a",
+    "url": "/static/media/landingpageimage.7abd4dd3.png"
+  },
+  {
+    "revision": "4e768b90f05d8244b15f1f75e4d3b219",
+    "url": "/static/media/questionmark.4e768b90.png"
+  }
+]);
